2b11e46286635b00c7621ad7ce866e2790a5acaa
nozomi-iida <iida19990106@gmail.com>
2022-04-20 08:10:45 +0900

    chore: make db for dev and test

--- server/app/models/setup.go
+++ server/app/models/setup.go
@@ -1,14 +1,23 @@
 package models
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=db user=deploy password=password port=5432 TimeZone=Asia/Shanghai"
+	dsn := fmt.Sprintf("host=%s dbname=%s user=%s password=%s port=%s TimeZone=Asia/Shanghai",
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_NAME"),
+		os.Getenv("DATABASE_USERNAME"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_PORT"),
+	)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
