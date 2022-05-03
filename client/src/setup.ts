dd05258920542bb5acfab38996abf1acf970c227
nozomi-iida <iida19990106@gmail.com>
2022-05-03 10:57:46 +0900

    set up prettier and eslint and remove tailwind

--- client/src/setup.ts
+++ client/src/setup.ts
@@ -1,5 +1,6 @@
+import "@testing-library/jest-dom/extend-expect";
+import "jest-extended";
+
 // FIXME
 // @ts-ignore
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
-import '@testing-library/jest-dom/extend-expect'
-import 'jest-extended';
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true
+    host: true,
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setup.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setup.ts",
+  },
+});
